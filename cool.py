734bf6f99d12ba853223c39418a6bfd79f91c927
-global <oosterwijkcools@gmail.com>
2024-06-05 11:20:58 -0300

    ahhh helll nawwwhahhw woahhhhooo

--- cool.py
+++ cool.py
@@ -1,3 +1,10 @@
+
+"""
+
+This program is asss bruhhhhh
+
+"""
+
 x=int (input("Num. 1: "))
 y=int (input("Num. 2: "))
 print (x+y)
---
+++ if/first.py
@@ -0,0 +1,3 @@
+x = input("x,y: ").split(",")
+
+print(int(x[0]) + int(x[1]))
